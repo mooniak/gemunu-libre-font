4ef0f7351bdb49fc9ed05c4d5d9c4c3ef5900362
paarandika <paarandika@gmail.com>
2015-11-05 17:35:40 +0530

    Added the build scripts

--- scripts/builder.sh
+++ scripts/builder.sh
@@ -12,16 +12,12 @@
 
 cd ../sources/sfd
 
-python ../../scripts/fontconvert StickNoBills-sinhala-0.sfd ../../sources --ufo
-python ../../scripts/fontconvert StickNoBills-sinhala-1.sfd ../../sources --ufo
-python ../../scripts/fontconvert StickNoBills-latin-1.sfd ../../sources --ufo
-python ../../scripts/fontconvert StickNoBills-latin-0.sfd ../../sources --ufo
-python ../../scripts/fontconvert StickNoBills-tamil-0.sfd ../../sources --ufo
-python ../../scripts/fontconvert StickNoBills-tamil-1.sfd ../../sources --ufo
+python ../../scripts/fontconvert Gemunu-sinhala-0.sfd ../../sources --ufo
+python ../../scripts/fontconvert Gemunu-sinhala-1.sfd ../../sources --ufo
 
 cd ../../scripts
-python merger.py ../masters/StickNoBills-Regular.ufo ../sources/StickNoBills-sinhala-0.ufo ../sources/StickNoBills-latin-0.ufo ../sources/StickNoBills-tamil-0.ufo
-python merger.py ../masters/StickNoBills-ExtraBold.ufo ../sources/StickNoBills-sinhala-1.ufo ../sources/StickNoBills-latin-1.ufo ../sources/StickNoBills-tamil-1.ufo
+# python merger.py ../masters/GemunuLibre-Regular.ufo
+# python merger.py ../masters/GemunuLibre-ExtraBold.ufo
 
 cd ../
 python build.py
