ed9f99b7674f05718d8f3eedd9af793da58a9441
paarandika <paarandika@gmail.com>
2015-11-29 20:30:22 +0530

    removed update-tests.sh

--- scripts/update-tests.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#Run this for automation: fswatch -0 sources/ | xargs -0 -n1 -I{} sh scripts/update-tests.sh
-cd ../sources/sfd/
-
-fontforge ../../scripts/fontconvert Gemunu-sinhala-0.sfd --otf
-fontforge ../../scripts/fontconvert Gemunu-sinhala-1.sfd --otf
-
-
-mv *otf ../../tests/fonts
