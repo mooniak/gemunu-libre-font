3c08c1eec529aeb7a717d943dadac3cea4aac51b
paarandika <paarandika@gmail.com>
2016-04-03 19:50:01 +0530

    finished ttf build

--- scripts/gfbuilder.sh
+++ scripts/gfbuilder.sh
@@ -11,18 +11,24 @@
 #!/bin/bash
 
 cd ../sources/sfd
-rm -rf ../Gemunu-sinhala-0.ufo ../Gemunu-sinhala-1.ufo ../Gemunu-sinhala-1-opt.ufo
-python ../../scripts/fontconvert Gemunu-sinhala-0.sfd ../../sources --ufo
-python ../../scripts/fontconvert Gemunu-sinhala-1.sfd ../../sources --ufo
-python ../../scripts/fontconvert Gemunu-sinhala-1-opt.sfd ../../sources --ufo
 
-rm -rf ../ttf-build
+python ../../scripts/unlink.py Gemunu-sinhala-0.sfd Gemunu-sinhala-0-temp.sfd
+python ../../scripts/unlink.py Gemunu-sinhala-1.sfd Gemunu-sinhala-1-temp.sfd
+python ../../scripts/unlink.py Gemunu-sinhala-1-opt.sfd Gemunu-sinhala-1-opt-temp.sfd
+
+python ../../scripts/fontconvert Gemunu-sinhala-0-temp.sfd ../../sources --ufo
+python ../../scripts/fontconvert Gemunu-sinhala-1-temp.sfd ../../sources --ufo
+python ../../scripts/fontconvert Gemunu-sinhala-1-opt-temp.sfd ../../sources --ufo
 
 cd ../../scripts
-rm -rf ../masters/*.ufo
-python merger.py ../masters/GemunuLibre-Regular.ufo ../sources/Gemunu-sinhala-0.ufo ../sources/Gemunu-latin-0.ufo
-python merger.py ../masters/GemunuLibre-Bold.ufo ../sources/Gemunu-sinhala-1.ufo ../sources/Gemunu-latin-1.ufo
-python merger.py ../masters/GemunuLibre-ExtraBold.ufo ../sources/Gemunu-sinhala-1-opt.ufo ../sources/Gemunu-latin-1.ufo
-
+rm -R -f ../ttf-build
+rm -R -f ../masters/*.ufo
+python merger.py ../masters/GemunuLibre-Regular.ufo ../sources/Gemunu-sinhala-0-temp.ufo ../sources/Gemunu-latin-0.ufo
+python merger.py ../masters/GemunuLibre-ExtraBold.ufo ../sources/Gemunu-sinhala-1-temp.ufo ../sources/Gemunu-latin-1.ufo
+python merger.py ../masters/GemunuLibre-Bold.ufo ../sources/Gemunu-sinhala-1-opt-temp.ufo ../sources/Gemunu-latin-1.ufo
 cd ../
 python gfbuild-s.py
+
+cd sources/sfd
+rm -R -f ../Gemunu-sinhala-0-temp.ufo ../Gemunu-sinhala-1-temp.ufo ../Gemunu-sinhala-1-opt-temp.ufo
+rm -R -f Gemunu-sinhala-0-temp.sfd Gemunu-sinhala-1-temp.sfd Gemunu-sinhala-1-opt-temp.sfd
